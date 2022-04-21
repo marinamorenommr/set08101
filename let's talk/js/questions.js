0a16ecde4e9d3c3830c6d8bae217217b4f3b0823
marinamorenommr <marinamorenommr@gmail.com>
2022-04-21 17:43:47 +0100

    commit quiz pages

---
+++ let's talk/js/questions.js
@@ -0,0 +1,117 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: "Do you usually talk to about your mental health to family and friends?",
+  answer: "Yes",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+  {
+  numb: 2,
+  question: "Do you feel like you have your thoughts, moods and life under control?",
+  answer: "Yes",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+  {
+  numb: 3,
+  question: "Do you feel like you're maintaining a healthy balance between leisure, myself, my career, physical activity, and those I care about?",
+  answer: "Yes",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+  {
+  numb: 4,
+  question: "Do you feel like you are struggling to focus at work or university?",
+  answer: "No",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+  {
+  numb: 5,
+  question: "Are you having any extreme emotions or mood swings?",
+  answer: "No",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+{
+  numb: 6,
+  question: "Do you feel relaxed most of the time?",
+  answer: "Yes",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+{
+  numb: 7,
+  question: "Regarding your sleeping habits, Do you usually wake up and go to bed at similar times every day?",
+  answer: "Yes",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+{
+  numb: 8,
+  question: "Do you have supportive friendships",
+  answer: "Yes",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+{
+  numb: 9,
+  question: "Do you sometimes feel alone or lonely?",
+  answer: "No",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+{
+  numb: 10,
+  question: "Do you usually feel heard?",
+  answer: "Yes",
+  options: [
+    "Yes",
+    "No"
+  ]
+},
+
+
+// you can uncomment the below codes and make duplicate as more as you want to add question
+// but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+//   {
+//   numb: 6,
+//   question: "Your Question is Here",
+//   answer: "Correct answer of the question is here",
+//   options: [
+//     "Option 1",
+//     "option 2",
+//     "option 3",
+//     "option 4"
+//   ]
+// },
+];
